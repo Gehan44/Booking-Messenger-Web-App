bf2b18cbbe553ef384850727c4f291a6ec75c7e2
Gehan44 <gehan.dub@gmail.com>
2024-02-23 15:50:42 +0700

    first commit

--- sqlConfig.js
+++ sqlConfig.js
@@ -1,13 +1,24 @@
+//module.exports = {
+//    server: 'localhost\\SQLEXPRESS',
+//    database: 'pine_bos',
+//    port: 1433,
+//    user: 'hansolo',
+//    password: 'DubeInBangkok2024',
+//    trustServerCertificate: true,
+//    options: {
+//        cryptoCredentialsDetails: {
+//            trustServerCertificate: true,
+//        }
+//    }
+//};
+
 module.exports = {
-    server: 'localhost\\SQLEXPRESS',
+    user: '',
+    password: '',
+    server: 'sql-db-pine-solution.public.f2b01b7c78ad.database.windows.net',
+    port: 3342,
     database: 'pine_bos',
-    port: 1433,
-    user: 'hansolo',
-    password: 'DubeInBangkok2024',
-    trustServerCertificate: true,
     options: {
-        cryptoCredentialsDetails: {
-            trustServerCertificate: true,
-        }
-    }
+      encrypt: true, // If you're using Azure, set this to true
+    },
 };
