a3693946ce69e02ee36ea6927bb98264a9869bb9
Gehan44 <gehan.dub@gmail.com>
2024-03-11 23:07:19 +0700

    first commit

--- sqlConfig.js
+++ sqlConfig.js
@@ -1,14 +1,5 @@
 require('dotenv').config();
 
-//module.exports = {
-//    server: 'localhost\\SQLEXPRESS',
-//    database: 'pine_bos',
-//    port: 1433,
-//    user: 'hansolo',
-//    password: 'DubeInBangkok2024',
-//    trustServerCertificate: true,
-//};
-
 module.exports = {
     user: 'dev_pine',
     password: process.env.DB_PASSWORD,
