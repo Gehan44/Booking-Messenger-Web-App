c1e40565552cfc93c1adaa2b5819a2e6f1a6884f
Gehan44 <gehan.dub@gmail.com>
2024-03-26 12:11:54 +0700

    first commit

--- sqlConfig.js
+++ sqlConfig.js
@@ -1,13 +1,13 @@
 require('dotenv').config();
 
 module.exports = {
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    server: process.env.DB_SERVER,
-    port: 1433,
-    database: 'pine_bos',
-    options: {
-      encrypt: true, // If you're using Azure, set this to true
-      trustServerCertificate: true,
-      }
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  server: process.env.DB_SERVER,
+  port: 1433,
+  database: 'pine_bos',
+  options: {
+    encrypt: true, // If you're using Azure, set this to true
+    trustServerCertificate: true,
+  }
 };
