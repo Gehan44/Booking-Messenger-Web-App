b2a42b4746df6506ee775c1a0ebcbe535f5b3254
Gehan44 <gehan.dub@gmail.com>
2024-05-02 14:25:44 +0700

    first commit

--- functions/storeTrackFunction.js
+++ functions/storeTrackFunction.js
@@ -10,20 +10,16 @@ module.exports = async (req, res) => {
         const createdDate = moment().format('YYYY-MM-DD');
         const createdTime = moment().format('HH:mm');
 
+        //Prevent Pastday
+        if (createdDate > requestDate) {
+            throw new Error('วันที่นัดไม่สามารถเป็นวันที่ผ่านมาได้');
+        }
+
         //Prevent Sameday
         if (createdDate === requestDate) {
             if (moment('16:00', 'HH:mm').isBefore(moment(createdTime, 'HH:mm'))) {
                 throw new Error('วันที่นัดไม่ทันส่งวันนี้แล้ว');
             }
-        }
-        
-        //Prevent Delay
-        const currentDate = new Date();
-        const requestDateMilliseconds = new Date(requestDate).getTime();
-        const differenceInMilliseconds =  requestDateMilliseconds - currentDate;
-        const twentyThreeDaysMilliseconds = 23 * 24 * 60 * 60 * 1000;
-        if (differenceInMilliseconds > twentyThreeDaysMilliseconds) {
-            throw new Error('ไม่สามารถกรอกวันที่นัดเกิน 23 วัน');
         }
 
         //Prevent Round
@@ -35,6 +31,15 @@ module.exports = async (req, res) => {
             if (moment(docTime,'HH:mm').isBefore(moment('13:00','HH:mm'))) {
                 throw new Error('กรุณากรอกเวลาให้ถูกต้อง');
             }
+        }
+
+        //Prevent Delay
+        const currentDate = new Date();
+        const requestDateMilliseconds = new Date(requestDate).getTime();
+        const differenceInMilliseconds =  requestDateMilliseconds - currentDate;
+        const twentyThreeDaysMilliseconds = 23 * 24 * 60 * 60 * 1000;
+        if (differenceInMilliseconds > twentyThreeDaysMilliseconds) {
+            throw new Error('ไม่สามารถกรอกวันที่นัดเกิน 23 วัน');
         }
         
         const hostname = req.headers.host;
